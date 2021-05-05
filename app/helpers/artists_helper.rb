5bc7ff15afd0be6592dba63c85418a15602d71b9
Jesse Wenger <wengerj@gmail.com>
2021-05-05 20:20:53 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  #Write a helper method #display_artist in the appropriate app/helpers file to be called on in our views. The method's return value should take into consideration whether an artist is already associated with a song:
+  #If an artist is already associated with the song, return a link to the artist's show page
+  #If an artist is not associated with the song (a.k.a. 'else'), return a link to the song's edit page, with a link text of "Add Artist"
+
+  def display_artist(song)
+    song.artist_name
+  end
 end
