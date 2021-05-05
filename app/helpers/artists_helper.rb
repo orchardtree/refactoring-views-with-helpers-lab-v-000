e90c9ec75b9d31d5eece7d05f69bc172dc849ac0
Jesse Wenger <wengerj@gmail.com>
2021-05-05 20:33:52 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
   #If an artist is not associated with the song (a.k.a. 'else'), return a link to the song's edit page, with a link text of "Add Artist"
 
   def display_artist(song)
-    if true == false
+    if song.artist.present?
       link_to song.artist_name, artist_path(song.artist_id)
     else
       link_to "Edit song to identify artist.", edit_song_path(song)
