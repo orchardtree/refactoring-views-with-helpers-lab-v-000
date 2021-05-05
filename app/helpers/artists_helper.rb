d1573e396de68e12e176b4a59e7094c838ee790f
Jesse Wenger <wengerj@gmail.com>
2021-05-05 20:32:35 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,6 +7,7 @@ module ArtistsHelper
     if true == true
       link_to song.artist_name, artist_path(song.artist_id)
     else
+      link_to "Edit song to identify artist.", song_path(song)
     end
   end
 end
