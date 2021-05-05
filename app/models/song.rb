68414967b405268352f8124044eace048fde43cb
Jesse Wenger <wengerj@gmail.com>
2021-05-05 20:45:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    if self.artist.present? ? self.artist.name : nil
+    self.artist.present? ? self.artist.name : nil
   end
 
   def artist_name=(name)
